5e18455a161cde1c4add2d78e8ee58fbcf652913
Samy Pessé <samypesse@gmail.com>
2015-08-20 23:02:56 +0200

    Fix test for pagination

--- test/pagination.js
+++ test/pagination.js
@@ -5,7 +5,7 @@ describe('Pagination', function() {
     it('should correctly return a Page object', function() {
         return client.repos()
         .then(function(page) {
-            page.should.have.property('current');
+            page.should.have.property('list');
             page.should.have.property('next');
             page.should.have.property('prev');
         });
